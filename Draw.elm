12c3362cc699e1434d94d5febff3042442857309
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2013-08-05 06:54:37 +0000

    dynamic engine drawing!

--- Draw.elm
+++ Draw.elm
@@ -4,36 +4,40 @@ import Graphics.Collage
 import Matrix2D (Matrix2D)
 import Matrix2D as M
 
-import Types (Part, Brain, FuelTank, Engine, Structure, Beam, Module, Attach)
+import Types (Part, Brain, FuelTank, Engine, Structure, Beam, Module, Attach, EngineConfig)
 
 translation : Float -> Float -> Matrix2D
 translation = M.matrix 1 0 0 1
 
-drawPart : Part -> Form
-drawPart part =
+drawPart : [EngineConfig] -> Part -> Form
+drawPart ecs part =
   case part of
     (Brain size) -> 
       filled blue <| circle size.r 
     (FuelTank size) -> 
       filled green <| rect size.w size.l 
     (Engine size) -> 
-      rotate 0.5 . filled red <| ngon 3 size.r 
+      group <|
+        (rotate 0.5 . filled red <| ngon 3 size.r) ::
+        (if any (\ec -> ec == size.config) ecs
+        then [ move (0,(0.7*size.r)) . rotate 0.5 . filled yellow <| ngon 3 (size.r * 0.7) ]
+        else [])
 
-drawAttachment : (Attach, Structure) -> Form
-drawAttachment ({offset,theta}, structure) =
+drawAttachment : [EngineConfig] -> (Attach, Structure) -> Form
+drawAttachment ec ({offset,theta}, structure) =
   let modelM = M.multiply (translation 0 offset) (M.rotation theta)
-      subForm = drawStructure structure
+      subForm = drawStructure ec structure
   in groupTransform modelM [subForm]
 
-drawBeam : Float -> [(Attach, Structure)] -> Form
-drawBeam length attachments =
+drawBeam : [EngineConfig] -> Float -> [(Attach, Structure)] -> Form
+drawBeam ec length attachments =
   let width = length * 0.05
       beamForm = move (0, length * 0.5) . filled gray <| rect width length
-  in group (beamForm :: map drawAttachment attachments) 
+  in group (beamForm :: map (drawAttachment ec) attachments) 
 
-drawStructure : Structure -> Form
-drawStructure structure =
+drawStructure : [EngineConfig] -> Structure -> Form
+drawStructure ec structure =
   case structure of
-    Module part -> drawPart part
-    Beam length attachments -> drawBeam length attachments
+    Module part -> drawPart ec part
+    Beam length attachments -> drawBeam ec length attachments
 
--- Main.elm
+++ Main.elm
@@ -1,8 +1,7 @@
 module Main where
 
-import Graphics.Collage
-
 import Draw (drawStructure)
+import Keyboard (wasd)
 import Types (Structure, Beam, Module, Part, Engine, Brain, FuelTank, EngineConfig, Thrust, TurnLeft, TurnRight, Attach)
 
 attitudeL : Structure
@@ -17,8 +16,8 @@ attitudeR = Beam 10 <|
 
 mods : [(Attach, Structure)]
 mods = 
-  [ ({ offset=45, theta=(pi/2) }, attitudeL)
-  , ({ offset=45, theta=(3*pi/2) }, attitudeR)
+  [ ({ offset=45, theta=(3*pi/2) }, attitudeL)
+  , ({ offset=45, theta=(pi/2) }, attitudeR)
   , ({ offset=0, theta=0 }, Module <| Brain {r=7})
   , ({ offset=25, theta=0 }, Module <| FuelTank {l=20,w=14})
   , ( { offset=50, theta=0 }, 
@@ -27,7 +26,25 @@ mods =
 simpleShip : Structure
 simpleShip = Beam 50 mods
 
-main : Element
-main =  
+control : { x:Int, y:Int } -> [ EngineConfig ]
+control input = 
+  (if input.y > 0 then [ Thrust ] else []) ++
+  (if input.x > 0 then [ TurnRight ] else []) ++
+  (if input.x < 0 then [ TurnLeft ] else [])
+
+ship : Signal Form
+ship = (\ec -> drawStructure ec simpleShip) <~ (control <~ wasd)
+
+space : Signal Element
+space = (\ship ->
   collage 400 300 <|
-    [ move (50,50) . rotate (pi/4) <| drawStructure simpleShip ]
+    [ filled black <| rect 400 300
+    , move (50,50) . rotate (pi/4) <| ship
+    ]) <~ ship
+
+main : Signal Element
+main = (\ses -> flow down <~ combine ses) <|
+  [ space 
+  , asText <~ wasd
+  , asText . control <~ wasd
+  ]
