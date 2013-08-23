bfaefe3a4d07beeb9219c60650c89372f75e06d4
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2013-08-23 03:30:04 +0000

    license, etc.

--- Main.elm
+++ Main.elm
@@ -1,3 +1,23 @@
+
+{-
+    Copyright (c) John P Mayer, Jr 2013
+
+    This file is part of celestia.
+
+    celestia is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    celestia is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with celestia.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 module Main where
 
 import Char (toCode)
--- run.ksh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/ksh
-
-node server.js
